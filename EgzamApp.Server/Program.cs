6d37dc34c4b2ec90d26d70133f143cffa93a3d94
Przemek <34289616+przemekbok@users.noreply.github.com>
2025-04-26 11:07:18 +0200

    Update Program.cs to register services and DB

--- EgzamApp.Server/Program.cs
+++ EgzamApp.Server/Program.cs
@@ -1,6 +1,15 @@
+using EgzamApp.Server.Data;
+using EgzamApp.Server.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=egzamapp.db"));
+
+// Register our custom services
+builder.Services.AddScoped<ExamService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +17,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Apply database migrations
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate();
+}
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
